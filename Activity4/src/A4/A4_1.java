549bf3561f9056ce2445b59d69c04fd79bfcc3de
Jonathan-Tabares <jonathan.tabares2222@gmail.com>
2023-01-30 18:55:58 +0100

    Upload1

---
+++ Activity4/src/A4/A4_1.java
@@ -0,0 +1,10 @@
+package A4;
+
+public class A4_1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Activity4/src/A4/A4_3.java
@@ -0,0 +1,11 @@
+package A4;
+
+public class A4_3 {
+
+	public static void main(String[] args) {
+		System.out.println("HeWo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
